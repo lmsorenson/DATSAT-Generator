49bdc28406b9be1b9914c1d135f1fa3f2d3dc233
Lucas Sorenson <lsorenson20@gmail.com>
2018-10-15 18:02:29 -0500

    add functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define FOOT (0)
+#define COLOR (1)
+#define DISTANCE (2)
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -8,7 +12,50 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+void MainWindow::GenerateDATSAT(QTableWidget * Table)
+{
+    for(int i=0; i < Table->columnCount(); i++)
+    {
+        for(int j=0; j < Table->rowCount(); j++)
+        {
+            Table->setItem(j,i,new QTableWidgetItem(GenerateText(i)));
+        }
+    }
+
+}
+
+QString MainWindow::GenerateText(int columnIndex)
+{
+    switch (columnIndex) {
+
+    default:
+    case FOOT:
+
+        return QString("Right");
+
+        break;
+
+    case COLOR:
+
+        return QString("Red");
+
+        break;
+
+    case DISTANCE:
+
+        return QString("Near");
+
+        break;
+    }
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_Generate_released()
+{
+    qDebug("Hello");
+    this->GenerateDATSAT(ui->tableWidget);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +15,12 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+    void GenerateDATSAT(QTableWidget * Table);
+    QString GenerateText(int columnIndex);
+
+private slots:
+    void on_Generate_released();
 
 private:
     Ui::MainWindow *ui;
